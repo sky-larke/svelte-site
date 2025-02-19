a9adbf4aa42d8619b842c11bf3d665d1e4b794f4
skye <67734042+sky-larke@users.noreply.github.com>
2025-02-18 21:18:11 -0800

    I need a linter. hotfix

--- src/routes/projects/+layout.svelte
+++ src/routes/projects/+layout.svelte
@@ -1,7 +1,3 @@
-<script context="module" lang="ts">
-    import Screen from "$lib/components/Screen.svelte";
-    // import Tiptap from '$lib/Tiptap.svelte';
-    import BlogHeader from '$lib/components/BlogHeader.svelte';
-</script>
+
 
 <slot></slot>

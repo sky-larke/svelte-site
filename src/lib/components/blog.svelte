f7daafd468cc6329fad5c5877398142d2e22a67d
skye <67734042+sky-larke@users.noreply.github.com>
2025-02-19 23:54:01 -0800

    Adding some formatting

--- src/lib/components/blog.svelte
+++ src/lib/components/blog.svelte
@@ -7,6 +7,8 @@
     export let subtitle: string = "";
     export let date: Date = new Date();
     export let tags: string[] = [];
+    export let thumbnail;
+    export let path:string ="";
 
     let breadcrumbs = [
         { label: "Projects", link: "" },
@@ -19,10 +21,11 @@
 </div>
 
 <main
-    class="flex-col flex-grow content
+    class="flex flex-col flex-grow content
         max-h-[90vh] pb-20 gap-[1em]
         sm:mx-[6em] mx-10 overflow-y-auto overflow-x-hidden no-scrollbar"
 >
+    <div class="flex flex-col items-center">
     <h1 class="mt-16">{title}</h1>
     <span class="subheading"
             >
@@ -33,6 +36,10 @@
     {/if}
     </span >
     <Tags {tags} />
+    {#if thumbnail}
+        <img src="{path}/{thumbnail}" alt="thumbnail for {title}">
+    {/if}
+    </div>
     <slot></slot>
 </main>
 
